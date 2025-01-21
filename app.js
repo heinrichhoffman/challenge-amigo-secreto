641576b067691c3da877ce49931156f9c6e71dd8
heinrichhoffman <hoffmanheinrich@gmail.com>
2025-01-21 18:17:42 -0300

    Version 1.0

--- app.js
+++ app.js
@@ -1,1 +1,56 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomes = [];
+
+function receiveName() {
+    let campo = document.getElementById("amigo");
+    let nome = campo.value.trim();
+
+    if (nome == "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    nomes.push(nome);
+    atualizarLista(); 
+    limparCampo();
+}
+
+function limparCampo() {
+    let campo = document.getElementById("amigo");
+    campo.value = "";
+}
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+
+    lista.innerHTML = "";
+
+    let i = 0;
+
+    
+    while (i < nomes.length) {
+        let item = document.createElement("li"); 
+        item.textContent = nomes[i]; // Define o texto como o nome do amigo
+        lista.appendChild(item); 
+        i++; 
+    }
+}
+
+function sortearAmigo() {
+    
+    if (nomes.length === 0) {
+        alert("Adicione um nome para sortear.");
+        return;
+    }
+
+    
+    let indiceAleatorio = Math.floor(Math.random() * nomes.length);
+    let amigoSorteado = nomes[indiceAleatorio];
+
+    
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>${amigoSorteado}</li>`;
+}
+
+
+
+
